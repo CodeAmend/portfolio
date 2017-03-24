81dc6f97452cb963c92382d2f8f83c62000879f2
Michael Bruce Allen <CodeAmend@Gmail.Com>
2017-03-24 17:07:36 -0500

    Moving to a React project | starter

---
+++ src/components/projects.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+const Projects = () => {
+  return(
+   <section id="projects">
+      <h4 className="ui horizontal divider header">
+         <a href="#">Projects</a>
+      </h4>
+   </section>
+  );
+};
+
+export default Projects;
