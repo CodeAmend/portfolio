3abfac521ba6322da8a01a2c9fa5a0210931e833
Michael Bruce Allen <CodeAmend@Gmail.Com>
2017-03-26 17:57:41 -0500

    Refactored index.js to have App component within

--- src/components/skills.js
+++ src/components/skills.js
@@ -4,50 +4,53 @@ import {connect} from 'react-redux';
 class Skills extends Component {
 
   renderSkills() {
-    return this.props.skills.map((skill) => {
-      return (
+    return this
+      .props
+      .skills
+      .map((skill) => {
+        return (
 
-        <article className="ui centered card">
-           <section className="content">
+          <article key={skill.title} className="ui centered card">
+            <section className="content">
               <a href="#" className="right floated meta">
-                 projects: 5
+                projects: 5
               </a>
               <h5 className="header">
-                 {skill.title}
+                {skill.title}
               </h5>
               {/* <div className="description">
                  One of my favorite inventions...
-              </div> */}
-           </section>
-           <aside className="extra">
+            </div> */}
+            </section>
+            <aside className="extra">
               Skill
               <section className="ui heart rating" data-rating={skill.level}></section>
-           </aside>
-        </article>
-      )
+            </aside>
+          </article>
+        )
 
-    });
+      });
   }
   render() {
-    return(
+    return (
       <section id="skills">
-         <h4 className="ui horizontal divider header">
-            <a href="#">Skills</a>
-         </h4>
-         <article className="ui container">
-            <section className="ui four doubling cards">
+        <h4 className="ui horizontal divider header">
+          <a href="#">Skills</a>
+        </h4>
+        <article className="ui container">
+          <section className="ui four doubling cards">
 
             {this.renderSkills()}
 
           </section>
-         </article>
+        </article>
       </section>
     );
   }
 }
 
 function mapStateToProps(state) {
-  return { skills: state.skills }
+  return {skills: state.skills}
 }
 
 export default connect(mapStateToProps)(Skills);
--- src/index.js
+++ src/index.js
@@ -3,10 +3,21 @@ import ReactDOM from 'react-dom';
 import {Provider} from 'react-redux';
 import {createStore} from 'redux';
 import rootReducer from './reducers/index';
-import App from './components/app';
+import Hero from './components/hero';
+import Projects from './components/projects';
+import Skills from './components/skills';
+
+const App = () => {
+  return (
+    <div>
+      <Hero/>
+      <Projects/>
+      <Skills/>
+    </div>
+  );
+}
 
 ReactDOM.render(
   <Provider store={createStore(rootReducer)}>
-    <App/>
-  </Provider>
-  , document.querySelector('#react'));
+  <App/>
+</Provider>, document.querySelector('#react'));
