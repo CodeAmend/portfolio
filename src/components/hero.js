933e07fcecbbfb3a5cc03765ab78ffc9bfa465fa
Michael Bruce Allen <CodeAmend@Gmail.Com>
2017-03-25 20:10:55 -0500

    Refactored Hero to renderLatestGitCommits()

--- src/components/hero.js
+++ src/components/hero.js
@@ -1,56 +1,69 @@
-import  React from 'react';
+import React, {Component} from 'react';
 
-const Hero = () => {
-  return (
-    <section id="hero">
-      <div className="ui container">
+class Hero extends Component {
 
-         <section className="ui equal width stackable grid">
+  renderLatestGitCommits() {
+    return (
+      <p>Commit 1</p>
+    );
+  }
+  render() {
+
+    return (
+      <section id="hero">
+        <div className="ui container">
+
+          <section className="ui equal width stackable grid">
 
             <article className="column">
 
-               <nav className="ui internally celled grid quad">
-                  <section className="equal width row">
-                     <article className="column project">Projects</article>
-                     <article className="column courses">Courses</article>
-                  </section>
+              <nav className="ui internally celled grid quad">
+                <section className="equal width row">
+                  <article className="column project">Projects</article>
+                  <article className="column courses">Courses</article>
+                </section>
 
-                  <section className="equal width row">
-                     <article className="column skills">Skills</article>
-                     <article className="column me">Me</article>
-                  </section>
-               </nav>
+                <section className="equal width row">
+                  <article className="column skills">Skills</article>
+                  <article className="column me">Me</article>
+                </section>
+              </nav>
 
-               <section className="row">
-                  <article className="column">
-                     <h1>Michael Bruce Allen</h1>
-                     <p>I have a strong interest to build booking systems, schedulars and social connection apps. Yeah! Stuff like that!</p>
-                     <p>I am a novice but...</p>
-                     <p>Here is what I am doing to get me to the next level.</p>
-                  </article>
-               </section>
+              <section className="row">
+                <article className="column">
+                  <h1>Michael Bruce Allen</h1>
+                  <p>I have a strong interest to build booking systems, schedulars and social connection apps. Yeah! Stuff like that!</p>
+                  <p>I am a novice but...</p>
+                  <p>Here is what I am doing to get me to the next level.</p>
+                </article>
+              </section>
 
             </article>
 
             <aside className="column">
-               <section className="ui segment">
+              <section className="updates ui segment">
+                <article className="current-course">
                   <h3>Current Course...</h3>
                   <p>Advanced React and Redux</p>
+                </article>
+                <article className="latest-course">
                   <h3>Last Finished Course...</h3>
                   <p>Understanding React and Redux</p>
+                </article>
+                <article className="latest-commits">
                   <h3>Latest Git Commits</h3>
-                  <p>Commit 1</p>
-                  <p>Commit 2</p>
-                  <p>Commit 3</p>
-               </section>
+                  {this.renderLatestGitCommits()}
+                </article>
+              </section>
             </aside>
 
-         </section>
+          </section>
 
-      </div>
-    </section>
+        </div>
+      </section>
 
-  );
+    );
+  }
 }
 
 export default Hero;
