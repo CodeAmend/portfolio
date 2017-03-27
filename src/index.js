bbe63f57ddb4ae48a8c07641521f09ddf951c66f
Michael Bruce Allen <CodeAmend@Gmail.Com>
2017-03-27 13:39:12 -0500

    Added Redux Thunk to index.js

--- src/index.js
+++ src/index.js
@@ -1,11 +1,13 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import {Provider} from 'react-redux';
-import {createStore} from 'redux';
+import {createStore, applyMiddleware} from 'redux';
+import reduxThunk from 'redux-thunk';
 import rootReducer from './reducers/index';
 import Hero from './components/hero';
 import Projects from './components/projects';
 import Skills from './components/skills';
+
 
 const App = () => {
   return (
@@ -17,7 +19,9 @@ const App = () => {
   );
 }
 
+const createStoreWithMiddleware = applyMiddleware(reduxThunk)(createStore);
+
 ReactDOM.render(
-  <Provider store={createStore(rootReducer)}>
+  <Provider store={createStoreWithMiddleware(rootReducer)}>
     <App/>
   </Provider>, document.querySelector('#react'));
