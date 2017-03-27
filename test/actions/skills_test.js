711106bca07ac970e4e65f8a45c1a73fa42128dd
Michael Bruce Allen <CodeAmend@Gmail.Com>
2017-03-27 11:05:15 -0500

    Renamed skills to skills_test

--- test/actions/skills_test.js
+++ test/actions/skills_test.js
@@ -1,10 +1,12 @@
 import {expect} from '../test_helper';
 import {fetchCommits} from '../../src/actions';
+import {FETCH_COMMITS} from '../../src/actions/types';
 
 describe("Skills action", () => {
 
   it("returns correct type", () => {
-    const request = fetchCommits();
-    console.log(request);
+    const action = fetchCommits();
+    console.log(FETCH_COMMITS);
+    expect(action.type).to.equal(FETCH_COMMITS)
   });
 });
