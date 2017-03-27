4482d0d783290dfc0328b8f037d48894831eb655
Michael Bruce Allen <CodeAmend@Gmail.Com>
2017-03-27 12:11:46 -0500

    Added GITHUB_API_TOKEN

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,6 +1,17 @@
+import {FETCH_COMMITS} from './types';
+import axios from 'axios';
+
+
+
 export function fetchCommits() {
+  const config = {
+    headers: {
+      Authorization: `Bearer ${process.env.GITHUB_API_TOKEN}`
+    }
+  }
+  const request = axios.get("https://api.github.com/users/codeamend/repos", config);
   return {
     type: FETCH_COMMITS,
-    payload: "data"
+    payload: request
   }
 }
--- test/actions/skills_test.js
+++ test/actions/skills_test.js
@@ -1,12 +1,27 @@
 import {expect} from '../test_helper';
-import {fetchCommits} from '../../src/actions';
+import {fetchCommits} from '../../src/actions/index';
 import {FETCH_COMMITS} from '../../src/actions/types';
 
 describe("Skills action", () => {
 
+  let action;
+
   it("returns correct type", () => {
-    const action = fetchCommits();
-    console.log(FETCH_COMMITS);
+    action = fetchCommits();
     expect(action.type).to.equal(FETCH_COMMITS)
   });
+
+  it("should return promise", () => {
+    action = fetchCommits();
+    expect(action.payload).to.be.a('promise');
+  });
+
+  it("should return correct info", (done) => {
+    action = fetchCommits();
+    action.payload.then((response) => {
+      console.log(response);
+      done();
+    })
+  });
+
 });
