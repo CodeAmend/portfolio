5ad36f69f3f9f724b425cf094e2de365614d6711
Michael Bruce Allen <CodeAmend@Gmail.Com>
2017-03-27 13:28:55 -0500

    Updated GITHUB_API_TOKEN to work without token

--- src/actions/index.js
+++ src/actions/index.js
@@ -4,14 +4,18 @@ import axios from 'axios';
 
 
 export function fetchCommits() {
+  const TOKEN = process.env.GITHUB_API_TOKEN || {};
   const config = {
     headers: {
-      Authorization: `Bearer ${process.env.GITHUB_API_TOKEN}`
+      Authorization: `Bearer ${TOKEN}`
     }
   }
+
   const request = axios.get("https://api.github.com/users/codeamend/repos", config);
+
   return {
     type: FETCH_COMMITS,
     payload: request
   }
+
 }
