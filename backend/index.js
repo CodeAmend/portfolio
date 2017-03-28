a6de5f75b92661563bddae2d243caddd9b86cfec
Michael Bruce Allen <CodeAmend@Gmail.Com>
2017-03-28 17:30:00 -0500

    Added basic skills api view

--- backend/index.js
+++ backend/index.js
@@ -10,6 +10,10 @@ app.get('/api', (req, res) => {
   res.send({ api: "portfolio website" });
 });
 
+app.get('/skills', (req, res) => {
+  res.send({ skill: "javascript" });
+});
+
 if (!module.parent) {
   app.listen(6464, () => {
     console.log("Backend API running...");
