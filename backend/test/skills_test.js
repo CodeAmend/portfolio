7c1142f600af5a3af8bd7a5df599cadb4e64045f
Michael Bruce Allen <CodeAmend@Gmail.Com>
2017-03-28 17:30:00 -0500

    Added basic skills api view

---
+++ backend/test/skills_test.js
@@ -0,0 +1,14 @@
+const expect = require('chai').expect;
+const request = require('supertest');
+const app = require('../index');
+
+describe("Skills", () => {
+  it("/skills GET should return json", (done) => {
+    request(app)
+      .get('/skills')
+      .end((err, response) => {
+        expect(response.body).to.eql({ skill: "javascript" })
+        done();
+      });
+  });
+});
