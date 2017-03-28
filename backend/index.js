051b0ea04fe5046f073b223b37407d890345f251
Michael Bruce Allen <CodeAmend@Gmail.Com>
2017-03-28 17:06:02 -0500

    Added backend app and test

--- backend/index.js
+++ backend/index.js
@@ -1,1 +1,19 @@
-import Express from 'express';
+const express = require('express');
+const mongoose = require('mongoose');
+const bodyParser = require('body-parser');
+
+const app = express();
+
+app.use(bodyParser.json());
+
+app.get('/api', (req, res) => {
+  res.send({ api: "portfolio website" });
+});
+
+if (!module.parent) {
+  app.listen(6464, () => {
+    console.log("Backend API running...");
+  });
+}
+
+module.exports = app;
---
+++ backend/test/app_test.js
@@ -0,0 +1,15 @@
+const expect = require('chai').expect;
+const request = require('supertest');
+const app = require('../index');
+
+
+describe("App", () => {
+  it("/api should return json", (done) => {
+    request(app)
+      .get('/api')
+      .end((err, response) => {
+        expect(response.body).to.eql({ api: "portfolio website" })
+        done();
+      });
+  });
+});
