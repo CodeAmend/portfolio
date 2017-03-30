c62e5394a93420a6e4a4a44c51a945f05451bb37
Michael Bruce Allen <CodeAmend@Gmail.Com>
2017-03-30 15:57:33 -0500

    Added required and unique to name of Skill model

--- backend/models/skill_schema.js
+++ backend/models/skill_schema.js
@@ -3,8 +3,27 @@ const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
 
 const SkillSchema = Schema({
-  name: String
+  name: {
+    type: String,
+    unique: true,
+    required: true
+  }
 });
+
+
+
+const handleE11000 = function(error, res, next) {
+  if (error.name === 'MongoError' && error.code === 11000) {
+    next(new Error('There was a duplicate key error'));
+  } else {
+    next();
+  }
+};
+
+SkillSchema.post('save', handleE11000);
+SkillSchema.post('update', handleE11000);
+SkillSchema.post('findOneAndUpdate', handleE11000);
+SkillSchema.post('insertMany', handleE11000);
 
 const Skill = mongoose.model('skill', SkillSchema);
 
