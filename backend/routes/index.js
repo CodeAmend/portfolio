6b943c9a57d936ed9e27378c20cc8bc165aef61a
Michael Bruce Allen <CodeAmend@Gmail.Com>
2017-03-31 12:39:45 -0500

    Created DELETE method for /api/skill

--- backend/routes/index.js
+++ backend/routes/index.js
@@ -10,16 +10,35 @@ module.exports = (app) => {
     res.send({ skill: "javascript" });
   });
 
-  app.post('/api/skill', (req, res) => {
+  app.put('/api/skill/:id', (req, res, next) => {
+    const _id = req.params.id;
+    const skillProps = req.body;
+    Skill.findByIdAndUpdate(
+      { _id }, skillProps, { new: true }
+    ).then((skill) => {
+      res.status(200).send(skill);
+    }).catch(next);
+  });
+
+  app.post('/api/skill', (req, res, next) => {
     const skill = new Skill(req.body);
     skill.save()
     .then((result) => {
       res.status(201).send(result);
-    }, () => {});
+    }).catch(next);
   });
 
   app.get('/api/project', (req, res) => {
     res.send(res.body);
+  });
+
+  app.delete('/api/skill/:id', (req, res, next) => {
+    const _id = req.params.id;
+    Skill.findByIdAndRemove(_id)
+    .then((skill) => {
+      if (!skill._id) res.status(404);
+      res.status(200).send({ message: "Deleted!" })
+    });
   });
 
 };
--- backend/test/models/skill_test.js
+++ backend/test/models/skill_test.js
@@ -14,9 +14,56 @@ describe("Skill API /skill", () => {
     .post('/api/skill')
     .send({ name: "javascript" })
     .end((err, response) => {
+
       expect(response.status).to.eql(201);
       expect(response.body).to.have.property('_id');
+      
       done();
+    });
+  });
+
+  it("PUT to /api/skill/:id update skill name", (done) => {
+    const skill = new Skill({ name: 'ajax' });
+    skill.save()
+    .then((result) => {
+      request(app)
+      .put(`/api/skill/${result.id}`)
+      .send({name: 'not ajax'})
+      .end((err, response) => {
+
+        expect(response.status).to.eql(200);
+
+        Skill.findOne({ _id: result.id})
+        .then((newSkill) => {
+
+          expect(newSkill.name).to.eql('not ajax');
+          expect(response.body._id).to.eql(result.id);
+
+          done();
+        });
+      });
+    });
+  });
+
+  it("DELETE /api/skill/:id to delete record and return ", (done) => {
+    const skill = new Skill({ name: 'Node.js'});
+    skill.save()
+    .then(() => {
+      request(app)
+      .delete(`/api/skill/${skill.id}`)
+      .end((err, response) => {
+
+        expect(response.status).to.eql(200);
+        expect(response.body).to.eql({ message: 'Deleted!' });
+
+        Skill.findById(skill.id)
+        .then((skill) => {
+
+          expect(skill).to.be.null;
+
+        });
+        done();
+      });
     });
   });
 
