5e271e6bac84f59e18805b55a68f9ebcb09775b7
Michael Bruce Allen <CodeAmend@Gmail.Com>
2017-04-01 11:24:54 -0500

    Added Skill array to project

--- backend/models/project_schema.js
+++ backend/models/project_schema.js
@@ -1,13 +1,17 @@
 const mongoose = require('mongoose');
-
 const Schema = mongoose.Schema;
+const SkillSchema = require('./skill_schema');
 
 const ProjectSchema = Schema({
   name: {
     type: String,
     unique: true,
     required: true
-  }
+  },
+  skill: [{
+    type: Schema.Types.ObjectId,
+    ref: 'skill'
+  }]
 });
 
 const Project = mongoose.model('project', ProjectSchema);
