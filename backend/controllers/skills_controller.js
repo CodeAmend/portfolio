db2bb0581fbc8a5f3ba88341a91ef6d2bd410faa
Michael Bruce Allen <CodeAmend@Gmail.Com>
2017-03-31 20:58:11 -0500

    REFACTOR: moved Skill API logic to controllers/skills_controller

---
+++ backend/controllers/skills_controller.js
@@ -0,0 +1,35 @@
+const Skill = require('../models/skill_schema');
+
+module.exports = {
+  index(req, res, next) {
+    res.send({ skill: "javascript" });
+  },
+
+  edit(req, res, next) {
+    const _id = req.params.id;
+    const skillProps = req.body;
+    Skill.findByIdAndUpdate(
+      { _id }, skillProps, { new: true }
+    ).then((skill) => {
+      res.status(200).send(skill);
+    }).catch(next);
+  },
+
+  create(req, res, next) {
+    const skill = new Skill(req.body);
+    skill.save()
+    .then((result) => {
+      res.status(201).send(result);
+    }).catch(next);
+  },
+
+  delete(req, res, next) {
+    const _id = req.params.id;
+    Skill.findByIdAndRemove(_id)
+    .then((skill) => {
+      if (!skill._id) res.status(404);
+      res.status(200).send({ message: "Deleted!" })
+    });
+  }
+
+}
--- backend/routes/index.js
+++ backend/routes/index.js
@@ -1,44 +1,18 @@
-const Skill = require('../models/skill_schema');
+const SkillControllers = require('../controllers/skills_controller');
 
 module.exports = (app) => {
-
-  app.get('/api', (req, res) => {
-    res.send({ api: "portfolio website" });
-  });
-
-  app.get('/api/skill', (req, res) => {
-    res.send({ skill: "javascript" });
-  });
-
-  app.put('/api/skill/:id', (req, res, next) => {
-    const _id = req.params.id;
-    const skillProps = req.body;
-    Skill.findByIdAndUpdate(
-      { _id }, skillProps, { new: true }
-    ).then((skill) => {
-      res.status(200).send(skill);
-    }).catch(next);
-  });
-
-  app.post('/api/skill', (req, res, next) => {
-    const skill = new Skill(req.body);
-    skill.save()
-    .then((result) => {
-      res.status(201).send(result);
-    }).catch(next);
-  });
-
-  app.get('/api/project', (req, res) => {
-    res.send(res.body);
-  });
-
-  app.delete('/api/skill/:id', (req, res, next) => {
-    const _id = req.params.id;
-    Skill.findByIdAndRemove(_id)
-    .then((skill) => {
-      if (!skill._id) res.status(404);
-      res.status(200).send({ message: "Deleted!" })
-    });
-  });
+  //
+  // app.get('/api', (req, res) => {
+  //   res.send({ api: "portfolio website" });
+  // });
+  //
+  // app.get('/api/project', (req, res) => {
+  //   res.send(res.body);
+  // });
+  //
+  app.get('/api/skill',SkillControllers.index);
+  app.put('/api/skill/:id', SkillControllers.edit);
+  app.post('/api/skill', SkillControllers.create);
+  app.delete('/api/skill/:id', SkillControllers.delete);
 
 };
