3ed58fde6dc7909a036302776b9bb394091ecdea
Michael Bruce Allen <CodeAmend@Gmail.Com>
2017-04-26 17:23:19 -0500

    Component: Projects component now renders proper fields

--- src/components/projects.js
+++ src/components/projects.js
@@ -1,49 +1,138 @@
-import React from 'react';
+import React, {
+  Component
+} from 'react';
+import {
+  connect
+} from 'react-redux';
 
-const Projects = () => {
-  return (
-    <section id="Projects">
-      <h4 className="ui horizontal divider header">
-        <a href="#">Projects</a>
-      </h4>
+class Projects extends Component {
 
-      <div className="ui container divided items">
+  renderProjects() {
+    return this.props.projects.map((project) => {
 
+      function renderProjectBullets() {
+        return project.bullets.map((bullet) => {
+          return (
+            <div className="item">{bullet}</div>
+          )
+        })
+      }
+
+      function renderProjectSkills() {
+        return project.skills.map((skill) => {
+          <div className="ui label">{skill.name}</div>
+        })
+      }
+
+      return (
         <div className="item">
           <div className="image">
             <img src="http://lorempixel.com/250/250/"></img>
           </div>
           <div className="content">
-            <a className="header">Portfolio Website</a>
+            <a className="header">{project.name}</a>
             <div className="meta">
-              <span className="cinema">React / Redux demo</span>
+              <span className="cinema">{project.tagline}</span>
             </div>
             <div className="description">
 
               <div className="ui bulleted list">
-
-                <div className="item">Put my React / Redux skills to the test</div>
-                <div className="item">Custom API to display data</div>
-                <div className="item">Sketches drawn on graph paper</div>
-
+                {renderProjectBullets()}
               </div>
 
             </div>
             <div className="extra">
-              <div className="ui label">React</div>
-              <div className="ui label">Node.js</div>
-              <div className="ui label">Express</div>
-              <div className="ui label">TDD</div>
-              <div className="ui label">Javascript</div>
-              <div className="ui label">Semantic UI</div>
+              {renderProjectSkills()}
+            </div>
+          </div>      }
+
+      function renderProjectSkills() {
+        return project.skills.map((skill) => {
+          <div className="ui label">{skill.name}</div>
+        })
+      }
+
+      return (
+        <div className="item">
+          <div className="image">
+            <img src="http://lorempixel.com/250/250/"></img>
+          </div>
+          <div className="content">
+            <a className="header">{project.name}</a>
+            <div className="meta">
+              <span className="cinema">{project.tagline}</span>
+            </div>
+            <div className="description">
+
+              <div className="ui bulleted list">
+                {renderProjectBullets()}
+              </div>
+
+            </div>
+            <div className="extra">
+              {renderProjectSkills()}
             </div>
           </div>
         </div>
+      )
+    })
+  }
 
-      </div>
+  render() {
 
-    </section>
-  );
+    return (
+      <section id="Projects">
+        <h4 className="ui horizontal divider header">
+          <a href="#">Projects</a>
+        </h4>
+
+        <div className="ui container divided items">
+
+          {this.renderProjects()}
+
+        </div>
+
+      </section>
+    );
+  }
 };
 
-export default Projects;
+function mapStateToProps(state) {
+  return {
+    projects: state.projects
+  }
+}
+
+export default connect(mapStateToProps)(Projects);
+
+        </div>
+      )
+    })
+  }
+
+  render() {
+
+    return (
+      <section id="Projects">
+        <h4 className="ui horizontal divider header">
+          <a href="#">Projects</a>
+        </h4>
+
+        <div className="ui container divided items">
+
+          {this.renderProjects()}
+
+        </div>
+
+      </section>
+    );
+  }
+};
+
+function mapStateToProps(state) {
+  return {
+    projects: state.projects
+  }
+}
+
+export default connect(mapStateToProps)(Projects);
